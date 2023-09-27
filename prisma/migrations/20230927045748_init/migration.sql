be1832d7e2bdf18144d653f53319a02558755624
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-09-26 21:59:22 -0700

    DB Setup

---
+++ prisma/migrations/20230927045748_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Calculator" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "inputA" DECIMAL NOT NULL,
+    "inputB" DECIMAL NOT NULL,
+    "result" DECIMAL NOT NULL,
+    "operand" TEXT NOT NULL
+);
